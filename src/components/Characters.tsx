267971e5e7b2d386b91dc62292d387b161399aab
MinoThauros <herimino@timatech.ca>
2022-11-29 16:00:21 -0500

    added pagination

--- src/components/Characters.tsx
+++ src/components/Characters.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import  {useQuery} from "react-query";
 import Character from './Character';
+import { Pagination } from '@mui/material';
 
 interface result{
         id:number,
@@ -22,8 +23,12 @@ const Characters:React.FC=()=>{
         },
         results:[]
     })
-    const fetchCharacters=async()=>{
-        const response=await fetch('https://rickandmortyapi.com/api/character')
+
+    const [queryIndex,setQueryIndex]=useState<number>(1)
+    const fetchCharacters=async(callbackReturns:any)=>{
+        console.log(callbackReturns)//we can see that the queryKeys are passed down to the function through the useQuery hook
+        const {queryKey}=callbackReturns
+        const response=await fetch(`https://rickandmortyapi.com/api/character?page=${queryKey[1]}`)
         return response.json()
         /**
          * before:
@@ -37,9 +42,11 @@ const Characters:React.FC=()=>{
         
     }
 
-    const {data,status}=useQuery("Characters",fetchCharacters)
+    const {data,status}=useQuery(["Characters",queryIndex],fetchCharacters)
     //linking a fetching function and a string key
     //useQuery wraps the fetching function
+    //we can add an index to the queryKey param in order to allow for pagination
+    //so that the queryIndex changes => makes caching easier
 
     if (status==="loading"){
         return <div>Loading...</div>
@@ -48,16 +55,27 @@ const Characters:React.FC=()=>{
         return <div>Error</div>
     }
 
+    const getLatestPage=(event: React.ChangeEvent<unknown>, page: number)=>{
+        setQueryIndex(page)
+    }
+
     
     
     return (
-        <div className='characters'>{/*classname makes the display flex */}
-            {data.results.map(((character:result)=>(
-                <Character character={character}/>
-                
-                
-                )))}
+        <div>
+            <div className='characters'>{/*classname makes the display flex */}
+                {data.results.map(((character:result)=>(
+                    <Character character={character}/>
+                    
+                    
+                    )))}
+                <div>
+                    <Pagination count={10} color="primary" onChange={getLatestPage} defaultPage={queryIndex}/>
+                </div>
+            </div>
+            
         </div>
+        
     )
 
     
